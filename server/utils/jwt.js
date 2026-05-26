be56c3b0c8d4b0910122db68316d588e77f09a50
Ekansh Lamba <ekanshlamba6226@gmail.com>
2026-05-27 03:50:27 +0530

    fix: production JWT access and refresh secrets resolution with debug logs

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,14 +1,20 @@
 const jwt = require('jsonwebtoken');
 
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || process.env.JWT_SECRET;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+
+console.log('JWT_ACCESS_SECRET length:', JWT_ACCESS_SECRET?.length);
+console.log('JWT_REFRESH_SECRET length:', JWT_REFRESH_SECRET?.length);
+
 const generateAccessToken = (user) => {
-  return jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, {
+  return jwt.sign({ userId: user._id, role: user.role }, JWT_ACCESS_SECRET, {
     expiresIn: '15m'
   });
 };
 
 const generateRefreshToken = (user, rememberMe = false) => {
   const expiresIn = rememberMe ? '30d' : '7d';
-  return jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn });
+  return jwt.sign({ userId: user._id }, JWT_REFRESH_SECRET, { expiresIn });
 };
 
 const sendRefreshTokenCookie = (res, token, rememberMe = false) => {
